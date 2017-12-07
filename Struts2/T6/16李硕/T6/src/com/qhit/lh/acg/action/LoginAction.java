6cad83720e3e5e732482408b49794c964ec1329b
Asa22 <2358353717@qq.com>
2017-12-07 11:36:58 +0800

    16李硕提交struts2 T6作业

---
+++ Struts2/T6/16李硕/T6/src/com/qhit/lh/acg/action/LoginAction.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.qhit.lh.acg.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class LoginAction extends ActionSupport {
+
+	/* (non-Javadoc)
+	 * @see com.opensymphony.xwork2.Action#execute()
+	 */
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
